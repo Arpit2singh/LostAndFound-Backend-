059630cd8024ffa0eb28c503e78154e7c57f2250
Arpit Singh <arpitlibono57@gmail.com>
2025-11-10 19:05:25 +0530

    ApiResponse and ApiError is added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ dotenv.config({
     path : "./.env"
 })
 import cors from "cors" ; 
-import express from "express"
+import express, { urlencoded } from "express"
 import DbLogic from "./Db/Db.js";
+import cookieParser from "cookie-parser";
 
 
 
@@ -18,5 +19,14 @@ app.use(cors({
       origin: process.env.CORS_ORIGIN || "*",
     credentials:true ,
 }))
+app.use(cookieParser()) ; 
+app.use(urlencoded({extended : true , limit : "25kb"}))
+app.use(express.json({limit:"25kb"}))
+
+
+//routes import 
+
+// routes declaration
+
 
 export default app;
---
+++ src/utils/ApiError.js
@@ -0,0 +1,12 @@
+
+class ApiError extends Error {
+    constructor(statusCode , message="Something went Wrong"  , error=[] , stack=""){
+        super(message) ; 
+        this.statusCode = statusCode ; 
+        this.message = message; 
+        this.error = error ;
+        this.success = false ; 
+        this.data = null ; 
+     }
+}
+export default ApiError  ;
